5dd78501a59d9a4b5c28b7eb24a8df968d48b3eb
Lia-Naomi <lia.hoida321@gmail.com>
2025-11-23 21:06:46 -0300

    :sparkles: Cria interface UsuarioRepository estendendo JpaRepository

--- src/main/java/com/unilopers/cinema/repository/UsuarioRepository.java
+++ src/main/java/com/unilopers/cinema/repository/UsuarioRepository.java
@@ -1,4 +1,13 @@
 package com.unilopers.cinema.repository;
 
-public interface UsuarioRepository {
+import com.unilopers.cinema.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
+
