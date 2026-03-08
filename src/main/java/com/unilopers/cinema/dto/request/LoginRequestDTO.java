3035b08b04363e71f558ef6535288b41e5b59b12
gustavomcfly <gustavomvictor00@gmail.com>
2026-03-08 19:29:58 -0300

    :sparkles: Define LoginRequestDTO e AuthResponseDTO para requisicao e resposta de autenticacao

---
+++ src/main/java/com/unilopers/cinema/dto/request/LoginRequestDTO.java
@@ -0,0 +1,24 @@
+package com.unilopers.cinema.dto.request;
+
+public class LoginRequestDTO {
+    private String email;
+    private String password;
+
+    public LoginRequestDTO() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/unilopers/cinema/dto/response/AuthResponseDTO.java
@@ -0,0 +1,26 @@
+package com.unilopers.cinema.dto.response;
+
+public class AuthResponseDTO {
+    private String token;
+    private String tipo = "Bearer";
+
+    public AuthResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
